d44dbadd6f6afb5cf1c5079fa037a25d8cccc438
Selina Amoah <125214809+samoah50@users.noreply.github.com>
2023-02-24 09:10:32 +0000

    Add files via upload

---
+++ carBrands.py
@@ -0,0 +1,37 @@
+#script for car dealers
+#carBrand with their respective prices in GHc
+carPrice = {'Mercedes-Benz':95670763,
+            'Ford': 45678908,
+            'Porsche':9856324,
+            'Tesla': 1234567894,
+           'BMW': 456721,
+           'Ferrari': 53467284,
+           'Honda': 87509346567,
+           'Toyota': 534289105,
+           'Lamborghini': 6734521890,
+           'Bentley': 567120957,
+           'Maserati': 4236789090,
+           'Audi':894563032,
+           'Jeep': 9805673,
+           'Subaru': 452789360,
+           'Cadillac': 546378912,
+           'Chrysler': 345789045,
+           'Hyundai': 9867345219,
+           'Dodge': 542378901,
+           'Mazda': 345669024,
+           'Jaguar': 56783902,
+           'Nissan': 674539021,
+           'Bugatti': 674907234,
+           'Buick': 9845621047,
+           'Alfa Romeo': 6572345190,
+           'Kia': 67389124,
+           'Volvo': 4562389,
+           'Mini': 123456789}
+name = input('Hello! valued customer,Welcome to JESSEY CAR DEALERS,Please specify the carbrand of your choice:')
+print('Your prefered carbrand is',name, 'which cost',carPrice[name],'.00 GHc')
+
+
+      
+
+ 
+
